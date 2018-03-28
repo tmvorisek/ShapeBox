77cd1543107682b7d36ebdc2e3b1c8b41c10f451
Camology <wirelessatari@gmail.com>
2018-03-27 19:12:59 -0800

    initial tests for circle

--- tests.cpp
+++ tests.cpp
@@ -4,6 +4,16 @@
 #include "catch.hpp"
 #include "Shape.h"
 
-TEST_CASE( "1: Circle", "[multi-file:1]" ) {
-  Circle(1);
+TEST_CASE( "Circle of positive radius height and width", "[multi-file:1]" ) {
+  REQUIRE (Circle(1).height() == 2);
+  REQUIRE (Circle(1).width() == 2);
+  REQUIRE (Circle(4).width() == 8);
+  REQUIRE (Circle(4).height() == 8);
+  REQUIRE (Circle(100).width() == 200);
+  REQUIRE (Circle(100).height() == 200);
+}
+
+TEST_CASE ( "Circle of negative radius") {
+  REQUIRE (Circle(-1).width() == -2.0);
+  REQUIRE (Circle(-1).height() == -2.0);
 }
