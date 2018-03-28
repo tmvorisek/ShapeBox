4f57962dcf612d85e7eb40417e21e669861de3c3
Camology <wirelessatari@gmail.com>
2018-03-27 20:09:24 -0800

    vertical height test passing

--- tests.cpp
+++ tests.cpp
@@ -22,9 +22,10 @@ TEST_CASE ( "Circle of negative radius") {
 }
 
 TEST_CASE ( "Vertical shape of 2 shapes height and width") {
-  std::vector<std::unique_ptr<Shape>> shapes{};
-  //std::unique_ptr<Shape> vertical = std::make_unique<Vertical>(shapes);
+  std::vector<std::unique_ptr<Shape>> shapes;
+  shapes.emplace_back(std::make_unique<Circle>(1));
+  shapes.emplace_back(std::make_unique<Circle>(5));
+  std::unique_ptr<Shape> vertical = std::make_unique<Vertical>(std::move(shapes));
   
-  Vertical vert = Vertical(shapes);
-  REQUIRE (vert.height() == 12);
+  REQUIRE (vertical->height() == 12.0);
 }
