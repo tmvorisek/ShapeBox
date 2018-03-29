5132a1d1a3ef31ce604c24998a62094343ab711e
Alan J Carvajal <alanjcarv@gmail.com>
2018-03-29 00:11:08 -0800

    finish horizontal and vertical

--- Shape.h
+++ Shape.h
@@ -72,6 +72,18 @@ private:
 	double _width, _height;
 };
 
+class Horizontal : public Shape
+{
+public:
+	Horizontal(std::vector<std::unique_ptr<Shape>> shapes);
+	virtual std::string toPostScript() const override;
+	virtual double width() const override;
+	virtual double height() const override;
+private:
+	std::vector<std::unique_ptr<Shape>> _shapes;
+	double _width, _height;
+};
+
 class Scale : public Shape
 {
 public:
